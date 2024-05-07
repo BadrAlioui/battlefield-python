ca5475a74b3a910a67978712746be6e93c847776
Badr Aliloui <tranquil2021@hotmail.com>
2024-05-07 09:02:12 +0000

    Add the option to keep playing witn and Add Exception ValueError

--- run.py
+++ run.py
@@ -15,8 +15,9 @@ print(
 )
 print('----------------------------------')
 
-name = input('Please enter your name: \n')
+name = input('Please enter your name: ')
 scores = {"computer": 0, "player": 0}
+
 
 def drawfield(field):
   for row in range(5):
@@ -28,12 +29,11 @@ def drawfield(field):
         print(" ", end="")
     print(' ')
 
-#create field for the player and the computer
+
 currentPlayer = [list(('-') * 5) for i in range(5)]
 
 currentComputer = [list(('-') * 5) for i in range(5)]
 
-#to hide the field or the board of the computer
 fieldCommun = [list(('-') * 5) for i in range(5)]
 
 for i in range(4):
@@ -54,62 +54,82 @@ print('----------')
 print('Computer\'s Board: ')
 print()
 
-def field(field):
-    '''
-    board for the computer without showing the ships
-    '''
-for row in range(5):
-  for col in range(10):
-    if col % 2 == 0:
-      rightColumn = col // 2
 
-      print(fieldCommun[row][rightColumn], end="")
-    else:
-      print(" ", end="")
-  print(' ')
+def field(field):
+  for row in range(5):
+    for col in range(10):
+      if col % 2 == 0:
+        rightColumn = col // 2
+
+        print(fieldCommun[row][rightColumn], end="")
+      else:
+        print(" ", end="")
+    print(' ')
 
 
 field(fieldCommun)
+print()
+playAgain = 'y'
+while playAgain == 'y':
+  while True:
+    try:
+      moveRow = int(input("Guess a row between 0 and 4: "))
+      moveColumn = int(input("Guess a column between 0 and 4: "))
+      if moveRow < 0 or moveRow > 4 or moveColumn < 0 or moveColumn > 4:
+        raise ValueError('Invalid input. Please enter a number between 0 and 4.')
+        
+      break
 
-for i in range(4):
-
-  moveRow = int(input("Enter the row you want to move to: "))
-  moveColumn = int(input("Enter the column you want to move to: "))
-  if currentComputer[moveRow][moveColumn] == '-':
+        
+    except ValueError:
+      print('Please enter a number between 0 and 4')
+  if currentComputer[moveRow][moveColumn] == '-' or currentComputer[moveRow][moveColumn] == 'X':
     currentComputer[moveRow][moveColumn] = 'X'
     fieldCommun[moveRow][moveColumn] = 'X'
     scores["player"] += 0
-    print(
-      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
-    )
 
   else:
     currentComputer[moveRow][moveColumn] = '$'
     fieldCommun[moveRow][moveColumn] = '$'
     scores["player"] += 1
-    print(
-      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
-    )
+  print()
+
+  print(f'Player guessed: ({moveRow}, {moveColumn})')
+  if fieldCommun[moveRow][moveColumn] == '$':
+    print('Player hit a ship!')
+  else:
+    print('Player missed! this time.')
+  print()
   field(fieldCommun)
   print('-------------------------')
   moveRow = random.randint(0, 4)
   moveColumn = random.randint(0, 4)
-  if currentPlayer[moveRow][moveColumn] == '-':
+  if currentPlayer[moveRow][moveColumn] == '-'or currentComputer[moveRow][moveColumn] == 'X':
     currentPlayer[moveRow][moveColumn] = 'X'
     scores["computer"] += 0
-    print(
-      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
-    )
+
   else:
     currentPlayer[moveRow][moveColumn] = '£'
     scores["computer"] += 1
-    
-    print(
-      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
-    )
+
+  print(f'Computer guessed: ({moveRow}, {moveColumn})')
+  if currentPlayer[moveRow][moveColumn] == '£':
+    print('Computer hit a ship!')
+  else:
+    print('Computer missed! this time.')
+
+  print('-----------------------------')
+  print()
 
   drawfield(currentPlayer)
+  print()
+  print(f"After this round, The score are: \n{name}: {scores['player']}.  Computer:  {scores['computer']}")
+  print()
+  print('Would you like to keep playing? (y/n)')
+  playAgain = input().lower()
 
+print()
+print('Thanks for playing!')
 print()
 drawfield(currentComputer)
 print('-------------------------')
