ce38a9290df815953429ee67f4e203be4a00968c
Badr Aliloui <tranquil2021@hotmail.com>
2024-05-07 08:57:33 +0000

    ask the player to target the ships

--- run.py
+++ run.py
@@ -58,15 +58,60 @@ def field(field):
     '''
     board for the computer without showing the ships
     '''
-  for row in range(5):
-    for col in range(10):
-      if col % 2 == 0:
-        rightColumn = col // 2
+for row in range(5):
+  for col in range(10):
+    if col % 2 == 0:
+      rightColumn = col // 2
 
-        print(fieldCommun[row][rightColumn], end="")
-      else:
-        print(" ", end="")
-    print(' ')
+      print(fieldCommun[row][rightColumn], end="")
+    else:
+      print(" ", end="")
+  print(' ')
 
 
 field(fieldCommun)
+
+for i in range(4):
+
+  moveRow = int(input("Enter the row you want to move to: "))
+  moveColumn = int(input("Enter the column you want to move to: "))
+  if currentComputer[moveRow][moveColumn] == '-':
+    currentComputer[moveRow][moveColumn] = 'X'
+    fieldCommun[moveRow][moveColumn] = 'X'
+    scores["player"] += 0
+    print(
+      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
+    )
+
+  else:
+    currentComputer[moveRow][moveColumn] = '$'
+    fieldCommun[moveRow][moveColumn] = '$'
+    scores["player"] += 1
+    print(
+      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
+    )
+  field(fieldCommun)
+  print('-------------------------')
+  moveRow = random.randint(0, 4)
+  moveColumn = random.randint(0, 4)
+  if currentPlayer[moveRow][moveColumn] == '-':
+    currentPlayer[moveRow][moveColumn] = 'X'
+    scores["computer"] += 0
+    print(
+      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
+    )
+  else:
+    currentPlayer[moveRow][moveColumn] = '£'
+    scores["computer"] += 1
+    
+    print(
+      f"The score is: {scores['computer']} for the computer and {scores['player']} for the player"
+    )
+
+  drawfield(currentPlayer)
+
+print()
+drawfield(currentComputer)
+print('-------------------------')
+
+drawfield(currentPlayer)
