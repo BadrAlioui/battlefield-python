eed5002b05565d952dc9e43803e985d79160b87a
Badr Aliloui <tranquil2021@hotmail.com>
2024-05-07 08:07:41 +0000

    Explain the game

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
-#- - - - -
-#- - - - -
-#- - - - -
-#- - - - -
-#- - - - -
+#build a board
 
-for row in range(5):
-    for column in range(10):
-        if column % 2 == 0:
-            print("-", end="")
-        else:
-            print(" ", end="")
-    print(" ")
+#0123456789
+#- - - - - 0
+#- - - - - 1
+#- - - - - 2
+#- - - - - 3
+#- - - - - 4
+
+print('----------------------------------')
+print(
+  'Welcome to Ultimate BATTLESHIPS! !\nBoard size: 5. Number of ships : 4\nTop left corner is row:, col: 0'
+)
+print('----------------------------------')
