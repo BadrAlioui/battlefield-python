f63925407b8ddcca1724cddff38b024807b43871
Badr Aliloui <tranquil2021@hotmail.com>
2024-05-07 08:00:30 +0000

    Create a board

--- run.py
+++ run.py
@@ -3,3 +3,11 @@
 #- - - - -
 #- - - - -
 #- - - - -
+
+for row in range(5):
+    for column in range(10):
+        if column % 2 == 0:
+            print("-", end="")
+        else:
+            print(" ", end="")
+    print(" ")
