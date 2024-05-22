5154feaf8ae124aaca1542df60dfbcee64b0a236
Badr Aliloui <tranquil2021@hotmail.com>
2024-05-22 09:47:09 +0000

    Add an option to permit to quit the game before the end of the game

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import random
 from colorama import Fore, init
+
 init(autoreset=True)
+
 
 # Function to draw the game board
 def drawfield(field):
@@ -28,6 +30,7 @@ def drawfield(field):
         print(" ", end="")
     print(' ')
 
+
 anotherGame = True
 while anotherGame:
 
@@ -35,27 +38,24 @@ while anotherGame:
   print('**********************************')
   print(
     "Welcome to Ultimate BATTLESHIPS! !\nBoard size: 5. Number of ships : 4\nTop left corner is row: 0, col: 0'\nAnd you have 5 turns to sink all the ships\nGood Luck!"
-  
   )
   print('**********************************')
-  
+
   # Prompt the user to enter their name
   while True:
     name = input(Fore.GREEN + "Enter your Name:\n")
     if not name.isalpha():
-        print (Fore.RED + "Enter only name with letters, please.")
+      print(Fore.RED + "Enter only name with letters, please.")
     else:
       break
   # Initialize the scores dictionary
   scores = {"computer": 0, "player": 0}
-  
-  
-  
+
   # Create arrays to represent the player and computer boards
   currentPlayer = [list(('-') * 5) for i in range(5)]
   currentComputer = [list(('-') * 5) for i in range(5)]
   fieldCommun = [list(('-') * 5) for i in range(5)]
-  
+
   # Function to generate random numbers for ship positions
   def random_number(gamer):
     '''
@@ -69,77 +69,74 @@ while anotherGame:
       movecompRow = row
       movecompColumn = col
       if row not in liste_set and col not in liste_set:
-  
+
         liste_set.add((movecompRow, movecompColumn))
         if gamer == currentPlayer:
           currentPlayer[movecompRow][movecompColumn] = "@"
         else:
           currentComputer[movecompRow][movecompColumn] = "'"
-  
+
   # Populate the boards with ships
   random_number(currentPlayer)
   random_number(currentComputer)
-  
+
   # Display the player's board
   print()
   print(f"{name}'s Board")
   print()
   drawfield(currentPlayer)
-  
+
   print()
   print('----------------------------------')
   print()
   print('Computer\'s Board: ')
   print()
-  
+
   # Display the computer's board with ships hidden
   drawfield(fieldCommun)
-  
+
   # Game play loop
   print()
   playAgain = 'y'
   tries = 0
-  while playAgain == 'y'and tries < 5:
-  
-    while True :
-  
+  while playAgain == 'y' and tries < 5:
+
+    while True:
+
       try:
-  
+
         moveRow = int(input("Guess a row between 0 and 4: \n"))
         moveColumn = int(input("Guess a column between 0 and 4: \n"))
-  
-  
-        if moveRow < 0 or moveRow > 4 or moveColumn < 0 or moveColumn > 4 :
-          raise ValueError(Fore.RED + 'Invalid input. Please enter a number between 0 and 4.')
-  
+
+        if moveRow < 0 or moveRow > 4 or moveColumn < 0 or moveColumn > 4:
+          raise ValueError(
+            Fore.RED + 'Invalid input. Please enter a number between 0 and 4.')
+
         break
-  
+
       except ValueError:
-        print(Fore.RED +'Please enter a number between 0 and 4')
+        print(Fore.RED + 'Please enter a number between 0 and 4')
     tries += 1
     if currentComputer[moveRow][moveColumn] == '-' or currentComputer[moveRow][
         moveColumn] == 'X':
       currentComputer[moveRow][moveColumn] = 'X'
       fieldCommun[moveRow][moveColumn] = 'X'
       scores["player"] += 0
-  
-  
-  
-  
+
     else:
       currentComputer[moveRow][moveColumn] = '$'
       fieldCommun[moveRow][moveColumn] = '$'
       scores["player"] += 1
-  
+
     print()
     print('**********************************')
     print(f'Player guessed: ({moveRow}, {moveColumn})')
     if fieldCommun[moveRow][moveColumn] == '$':
-  
+
       print('Player hit a ship!')
       print('**********************************')
     else:
-  
+
       print('Player missed this time.')
       print('**********************************')
     print()
@@ -151,45 +148,52 @@ while anotherGame:
         moveColumn] == 'X':
       currentPlayer[moveRow][moveColumn] = 'X'
       scores["computer"] += 0
-  
+
     else:
       currentPlayer[moveRow][moveColumn] = '£'
       scores["computer"] += 1
-  
+
     print(f'Computer guessed: ({moveRow}, {moveColumn})')
     if currentPlayer[moveRow][moveColumn] == '£':
       print('Computer hit a ship!')
     else:
       print('Computer missed this time.')
-  
+
     print('**********************************')
     print()
-  
+
     drawfield(currentPlayer)
     print()
-  
-    print(Fore.CYAN + 
-        f"After this round, The scores are: \n{name}: {scores['player']}.  Computer:  {scores['computer']}"
-      )
-  
-  
+
+    print(
+      Fore.CYAN +
+      f"After this round, The scores are: \n{name}: {scores['player']}.  Computer:  {scores['computer']}"
+    )
+
     print()
     if tries < 5:
       while True:
         try:
-  
-            playAgain = input('Would you like to keep playing? (y/n)\n').lower()
-            if playAgain != 'y' and playAgain != 'n':
-              raise ValueError(Fore.RED + "Please enter 'y' or 'n'")
-            else:
-              break
-  
+
+          playAgain = input('Would you like to keep playing? (y/n)\n').lower()
+          if playAgain != 'y' and playAgain != 'n':
+            raise ValueError(Fore.RED + "Please enter 'y' or 'n'")
+          elif playAgain == 'n':
+
+            break
+            anotherGame = False
+          else:
+            break
+
         except ValueError:
           print(Fore.RED + "Please enter 'y' or 'n'")
     else:
       print(Fore.RED + 'You have run out of tries!')
-  
-      print(Fore.CYAN + f"Finally, scores are: \n{name}: {scores['player']}.  Computer:  {scores['computer']}")
+
+      print(
+        Fore.CYAN +
+        f"Finally, scores are: \n{name}: {scores['player']}.  Computer:  {scores['computer']}"
+      )
       if scores['player'] > scores['computer']:
         print(Fore.GREEN + f"Congratulations {name}! You won!")
       elif scores['player'] < scores['computer']:
@@ -197,7 +201,7 @@ while anotherGame:
       else:
         print(Fore.YELLOW + "It is a DRAW!")
       break
-  
+
   print()
   print('Thanks for playing!')
   print()
@@ -206,6 +210,9 @@ while anotherGame:
   drawfield(currentPlayer)
   print()
   while True:
+    if playAgain == 'n':      
+      anotherGame = False
+      break
     anotherGame = input('Would you like to play another game? (y/n)\n').lower()
     if anotherGame != 'y' and anotherGame != 'n':
       print(Fore.RED + "Please enter 'y' or 'n'")
